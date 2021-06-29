a8aaab3fbe386b43dc0a9e0d454cea461d62b42c
Erza Putra Albasori <erzaputra9110@gmail.com>
2021-06-29 20:22:16 +0700

    add: single item imagery

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cons = require('consolidate');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,9 @@ var app = express();
 app.engine('html', cons.swig);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+
+// cors setup
+app.set(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
